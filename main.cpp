fd1b8dc9f028b04faabf7304d4b0cac68daf8c28
SteponasK <132608893+SteponasK@users.noreply.github.com>
2023-10-17 19:52:25 +0300

    Cleaned up main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,26 +1,24 @@
 #include "Header.h"
 #include "Textbook.h"
 #include "ignoreLine.h"
+
 std::string FILE_NAME = "tasks.txt";
+
 int main() {
+  Textbook textbook;
+  // Return 1 - File not created
+  if (textbook.File_To_Vector()) {
+    textbook.Create_File();
+  }
+  // Return 1 - Vector Empty
+  if (textbook.Check_Vector_Size()) {
+    std::cout << "Type in your first task: ";
+    textbook.Create_Task();
+  }
 
-    Textbook textbook;
+  textbook.viewTasks();
 
-    if (textbook.File_To_Vector()) // Return 1 - File not created
-    {
-        textbook.Create_File();
-    }
+  textbook.Vector_To_File();
 
-    if (textbook.Check_Vector_Size()) // Return 1 - Vector Empty 
-    {
-        std::cout << "Type in your first task: ";
-        textbook.Create_Task();
-    }
-
-    textbook.viewTasks();
-
-    textbook.Vector_To_File();
-
-    return 0;
+  return 0;
 }
-
